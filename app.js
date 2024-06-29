9ed279a93cfe45d71ccd2f3854a52c4c2882bf21
ismaelGarcia23 <ismaelhernan1237@gmail.com>
2024-06-28 22:22:15 -0600

    cmabios isma

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from 'express';
-import router from './routes/usuarioRoute.js';
+import usuarioRoute from './routes/usuarioRoute.js';
 
 // Crear la app
 const app = express();
 
-/// pug
+
+//Habilitar pug
 app.set('view engine', 'pug');
+app.set('views', './views'); //indica donde se encuentran las vistas
 
-app.set('views', './views')
+//Carpeta publica (archivos estaticos)
+app.use(express.static('public')); //indica donde se encuentran los archivos estaticos
 
-///Carpeta publica
-app.use( express.static('public'));
 
 //Routing
-app.use('/auth', router);
+app.use('/auth', usuarioRoute);
+
 
 //Definir un puerto y arrancar proyecto
 const port = 3000;
