9337713251b411dbeab5e658c9afc1d5b5641e0a
MrxSteve <steventobar38@gmail.com>
2024-07-02 23:14:37 -0600

    agregando rutas para los formularios

--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {formularioLogin, formularioRegistro} from '../controllers/userController.js'
+import {formularioLogin, formularioRegistro, formularioOlvidePassword} from '../controllers/userController.js'
 const router = express.Router();
 
 //Routing
 
 router.get('/login', formularioLogin);
 router.get('/registro', formularioRegistro);
+router.get('/olvide-password', formularioOlvidePassword);
 
 
 export default router;
