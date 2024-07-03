8ca413cbf441977a53a0f665b2a6b5eba61fc0f6
MrxSteve <steventobar38@gmail.com>
2024-07-02 23:12:54 -0600

    agregando Controladores de registro, login, y forgot pass

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,24 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
+        pagina: 'Iniciar Sesion',
         
     });
 };
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-        
+        pagina: 'Crear Cuenta',
+    });
+};
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recuperar Contraseña',
     });
 };
 
 export {
     formularioLogin,
     formularioRegistro,
+    formularioOlvidePassword,
 }
